16bccc57d314ce398fd9c1a98defc1a1ff5f6c9a
Илья Вилков <enot19vilkov@yandex.ru>
2025-09-19 16:44:23 +0300

    Add Actions

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,8 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '84.201.178.169',
-                 'taskihelper.sytes.net']
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
 
 # Application definition
 
